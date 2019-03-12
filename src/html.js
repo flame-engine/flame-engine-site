44e5a96b03ec66b9a8732d66ed4d3be77404b89e
Renan Araujo <renan.araujo@dextra-sw.com>
2019-03-12 09:53:08 -0300

    gh-pages

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <title> Flame </title>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
