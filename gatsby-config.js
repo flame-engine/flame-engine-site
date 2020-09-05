c841bd0437e09ed26ca63dbba1504a4efaf2879d
renancaraujo <renan.araujo@dashdash.com>
2020-09-05 15:29:45 +0100

    remove styled

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-fontawesome-css`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
