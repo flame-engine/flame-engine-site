3f9b1c48e4b04f9ec7b85efaae35f18c2754a83b
Renan <renancaraujo@users.noreply.github.com>
2020-09-16 15:36:18 +0100

    add redirects (#12)

---
+++ src/templates/discordTemplate.js
@@ -0,0 +1,2 @@
+import createRedirect from "../components/redirect"
+export default createRedirect("https://discord.gg/pxrBmy4")
---
+++ src/templates/merchTemplate.js
@@ -0,0 +1,2 @@
+import createRedirect from "../components/redirect"
+export default createRedirect("https://teespring.com/stores/flameengine")
