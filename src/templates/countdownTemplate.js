776f5108e10cc925714444dfc005c82135c56c87
Luan Nico <luanpotter27@gmail.com>
2020-12-02 21:53:05 -0500

    Fix and improve countdown

--- src/templates/countdownTemplate.js
+++ src/templates/countdownTemplate.js
@@ -6,37 +6,46 @@ import { faFire } from "@fortawesome/free-solid-svg-icons"
 
 import "./countdown.css"
 
+const now = () => new Date().getTime()
+
 export default function Template() {
   const SEC_IN_MILLIS = 1000
   const MIN_IN_SEC = 60
 
-  const [counter, setCounter] = useState(null)
+  const [currentTime, setCurrent] = useState(null)
+  const [targetTime, setTarget] = useState(null)
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search)
-    const currentTimestamp = new Date().getTime()
-    const targetTimestamp = urlParams.get("timestamp") || currentTimestamp
-    const delta = (targetTimestamp - currentTimestamp) / SEC_IN_MILLIS
-    setCounter(delta)
+    const currentTimestamp = now()
+    const tMinus = urlParams.get("tminus")
+    const targetTimestamp = tMinus
+      ? currentTimestamp + tMinus * SEC_IN_MILLIS
+      : urlParams.get("timestamp") || currentTimestamp
+    console.log(targetTimestamp)
+    setCurrent(currentTimestamp)
+    setTarget(targetTimestamp)
   }, [])
 
   useEffect(() => {
-    if (counter) {
-      setTimeout(() => setCounter(counter - 1), SEC_IN_MILLIS)
+    if (currentTime) {
+      setTimeout(() => setCurrent(now()), SEC_IN_MILLIS)
     }
-  }, [counter])
+  }, [currentTime])
 
   const seconds = num =>
     `${Math.abs(Math.round(num % MIN_IN_SEC))}`.padStart(2, "0")
-  const minutes = num => Math.ceil(num / MIN_IN_SEC)
-  const format = num => (num ? `${minutes(num)}:${seconds(num)}` : "-")
+  const minutes = num => Math.floor(num / MIN_IN_SEC)
+  const format = num =>
+    num && num > 0 ? `${minutes(num)}:${seconds(num)}` : "-"
 
+  const delta = Math.round((targetTime - currentTime) / SEC_IN_MILLIS)
   return (
     <PageWrapper title="Countdown" fullWitdh={true}>
       <div className="flame-logo-wrapper">
         <div className="flame-logo-inner countdown">
           <FontAwesomeIcon icon={faFire}></FontAwesomeIcon>
-          <p>{format(counter)}</p>
+          <p>{format(delta)}</p>
         </div>
       </div>
     </PageWrapper>
