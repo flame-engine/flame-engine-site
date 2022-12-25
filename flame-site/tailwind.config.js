6894dffff567ab3ac7bdb0ac6afef9574f3a8889
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2022-12-25 15:33:46 +0100

    start

---
+++ flame-site/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+    //   './_drafts/**/*.html',
+      './_includes/**/*.html',
+      './_layouts/**/*.html',
+    //   './_posts/*.md',
+      './*.md',
+      './*.html',
+    ],
+    theme: {
+      theme: {
+        extend: {},
+      },
+    },
+    plugins: []
+  }
+  
